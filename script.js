1221960e11f0d22b61a9d40efb82c51446acec31
AMAR KUMAR CHOUDHARY <98702824+amark4642@users.noreply.github.com>
2022-03-14 17:01:42 +0530

    Add files via upload
    back buttons added and results will be displayed in list

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 const apiUrl = `https://api.lyrics.ovh`;
-var searchBox, artist, title;
+//  searchBox, artist, title;
 
 
 
@@ -18,24 +18,43 @@ async function searchSongs() {
         }
     }
     else {
-    const data = await res.json();
-    const arrayData = data.data;
-    title = arrayData[0].title;
-    artist = arrayData[0].artist.name;
-    write(arrayData);
-}
+        const data = await res.json();
+        const arrayData = data.data;
+        // title = arrayData[0].title;
+        // artist = arrayData[0].artist.name;
+        write(arrayData);
+    }
 }
 
+var resultList = "";
+function write(data) {
+    let temp = "";
+    for (let i = 0; i < data.length; i++) {
+        const a = data[i].artist.name;
+        const b = data[i].title;
+        // console.log(data[i].artist.name,data[i].title);
+        // temp = temp + `<li>${data[i].title}-${data[i].artist.name}-<button onclick="getLyrics(${data[i].artist.name},${data[i].title})" class="myButton" id="${i}">Get Lyrics</button></li>`
+        temp = temp + `<li><span class="a">${data[i].title}-${data[i].artist.name}</span><span class="b"><button onclick="getLyrics('${a}','${b}')" class="myButton" id="${i}">Get Lyrics</button></span></li>`
+    }
+    resultList = temp;
+    document.getElementById("result").innerHTML = `${temp}<br><button class="backbtn" onclick="goBack()">Back</button>`;
 
-function write(data)
+    // document.getElementById("").addEventListener("click", getLyrics);
+
+}
+
+function goBack()
 {
-    document.getElementById("result").innerHTML =
-        `<span><h2>${data[0].title} - ${data[0].artist.name}</span> - <button id="newButton">Get Lyrics</button>`;
-        document.getElementById("newButton").addEventListener("click", getLyrics);
-                
+    document.getElementById("result").innerHTML = `Results will be displayed here..`;
 }
 
-async function getLyrics() {
+function goBacktoResult(temp)
+{
+    document.getElementById("result").innerHTML = `${resultList}<br><button class="backbtn" onclick="goBack()">Back</button>`;
+}
+
+async function getLyrics(artist,title) {
+    console.log(artist, title);
     const res = await fetch(`${apiUrl}/v1/${artist}/${title}`);
     if (res.status >= 400) {
         prompt("Not Found !", "No such song in the database ! Sorry !");
@@ -43,8 +62,9 @@ async function getLyrics() {
     }
     else {
         const data = await res.json();
+        const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
         document.getElementById("result").innerHTML =
-            `<span><div>Lyrics of - "<strong>${searchBox}</strong>"</div><br>${data.lyrics}</span>`;
+            `<span><div>Lyrics of - "<strong>${searchBox}</strong>"</div><br>${lyrics}</span><br><button class="backbtn" onclick="goBacktoResult()">Back</button>`;
     }
 }
 
